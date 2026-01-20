4f671993a498fb855f56e4fd280555672c9fcda7
Carlos <carlos.aquinore@alumno.buap.mx>
2026-01-20 09:42:04 -0600

    rutas relativas a absolutas dashboard

--- sistema-monitoreo/src/pages/DashboardCard.jsx
+++ sistema-monitoreo/src/pages/DashboardCard.jsx
@@ -1,9 +1,8 @@
 import { Grid, Typography, Box, CircularProgress } from '@mui/material';
-import { useStatusActions } from '../hooks/useStatusActions';
-import { useObtenerServidores } from '../hooks/useObtenerServidores.jsx'; // Usando ruta relativa si el alias @ falla
-//import { actualizarEstadoServidor } from '../api/actualizarEstadoServidor.js';
-import { SeccionResumen } from '../components/SeccionResumen';
-import { ListadoServidores } from '../components/ListadoServidores';
+import { useStatusActions } from '@hooks/useStatusActions';
+import { useObtenerServidores } from '@hooks/useObtenerServidores.jsx'; 
+import { SeccionResumen } from '@components/SeccionResumen';
+import { ListadoServidores } from '@components/ListadoServidores.jsx';
 import {useServidorLevantar} from "@hooks/useServidorLevantar.jsx";
 
 const DashboardCard = () => {
@@ -22,11 +21,7 @@ const DashboardCard = () => {
     return (
         <Box sx={{ p: 4 }}>
             <Typography variant="h4" sx={{ mb: 4 }}>Monitoreo SIMF (Dinámico)</Typography>
-
-            {/* 1. Módulo de Resumen */}
             <SeccionResumen servidores={servidores} />
-
-            {/* 2. Módulo de Listado */}
             <ListadoServidores
                 servidores={servidores}
                 actionLoading={actionLoading}
