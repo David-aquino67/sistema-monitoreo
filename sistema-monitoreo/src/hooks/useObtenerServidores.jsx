7e5718fc2b662d888d70bbce6d21eb591d422214
Carlos <carlos.aquinore@alumno.buap.mx>
2026-01-20 10:19:17 -0600

    rutas relativas - absolutas useobtenerservidores

--- sistema-monitoreo/src/hooks/useObtenerServidores.jsx
+++ sistema-monitoreo/src/hooks/useObtenerServidores.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useCallback } from 'react';
-import { obtenerServidores } from '../api/Obtenerservidores.jsx';
+import { obtenerServidores } from '@api/Obtenerservidores.jsx';
 
 export const useObtenerServidores = () => {
     const [servidores, setServidores] = useState([]);
