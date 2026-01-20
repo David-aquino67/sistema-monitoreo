f2286debc624217aed6c9b7420fc0ad928596576
Carlos <carlos.aquinore@alumno.buap.mx>
2026-01-20 11:44:17 -0600

    iconos en statusresumen

--- sistema-monitoreo/src/components/StatusResume/StatusResume.jsx
+++ sistema-monitoreo/src/components/StatusResume/StatusResume.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 import { Box, Paper, Typography } from '@mui/material';
 import { mapaEstados } from '@/const/StatusResume/mapaEstados.js';
 import { estilosStatus } from '@styles/StatusResume/StatusResume.jsx';
-
+import WarningIcon from '@mui/icons-material/Warning';
+import ConstructionIcon from '@mui/icons-material/Construction';
+import DangerousIcon from '@mui/icons-material/Dangerous'; 
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 const StatusResume = ({ quantity = 0, status = 'online' }) => {
     const config = mapaEstados[status] || { color: '#ccc', etiqueta: 'Desconocido' };
 
@@ -15,13 +18,24 @@ const StatusResume = ({ quantity = 0, status = 'online' }) => {
                 >
                     {config.etiqueta}
                 </Typography>
-
-                <Box sx={estilosStatus.indicador(config.color)} />
+            {status === 'warning' && (
+                <WarningIcon sx={{ color: config.color, fontSize: '1.5rem', ml: 1 }} />
+            )}
+            {status === 'maintenance' && (
+                <ConstructionIcon sx={{ color: config.color, fontSize: '1.5rem', ml: 1 }} />
+            )}
+            {status === 'offline' && (
+                <DangerousIcon sx={{ color: config.color, fontSize: '1.5rem', ml: 1 }} />
+            )}
+            {status === 'online' && (
+                <CheckCircleIcon sx={{ color: config.color, fontSize: '1.5rem', ml: 1 }} />
+            )}
             </Box>
 
             <Typography variant="h5" sx={estilosStatus.textoCantidad}>
                 {quantity}
             </Typography>
+            
         </Paper>
     );
 };
