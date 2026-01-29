b2fe582bf06fab219b5009686de732a7edb3c7f3
Carlos <carlos.aquinore@alumno.buap.mx>
2026-01-29 08:59:00 -0600

    configuracion de .env y echo

--- sistema-monitoreo/src/lib/echo.js
+++ sistema-monitoreo/src/lib/echo.js
@@ -5,7 +5,7 @@ window.Pusher = Pusher;
 
 export const echo = new Echo({
     broadcaster: 'reverb',
-    key: import.meta.env.VITE_REVERB_APP_KEY, // Debe estar en tu .env de React
+    key: import.meta.env.VITE_REVERB_APP_KEY || 'enh8pmyajb8m6qvbgmv',
     wsHost: import.meta.env.VITE_REVERB_HOST || 'localhost',
     wsPort: import.meta.env.VITE_REVERB_PORT || 8080,
     forceTLS: false,
