386d82c2ae86f64eee81b544da93e157354c81ac
Carlos <carlos.aquinore@alumno.buap.mx>
2026-02-05 06:03:30 -0600

    cambio de key

--- sistema-monitoreo/src/lib/echo.js
+++ sistema-monitoreo/src/lib/echo.js
@@ -5,7 +5,7 @@ window.Pusher = Pusher;
 
 export const echo = new Echo({
     broadcaster: 'reverb',
-    key: import.meta.env.VITE_REVERB_APP_KEY || 'enh8pmyajb8m6qvbgmv',
+    key: import.meta.env.VITE_REVERB_APP_KEY || 'enh8pmyajbp8m6qvbgmv',
     wsHost: import.meta.env.VITE_REVERB_HOST || 'localhost',
     wsPort: import.meta.env.VITE_REVERB_PORT || 8080,
     forceTLS: false,
