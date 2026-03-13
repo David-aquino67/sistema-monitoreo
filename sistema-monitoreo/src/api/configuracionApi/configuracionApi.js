f54bb9bd856dd9bc887b4fecbc4d30f14cba90a1
Carlos <carlos.aquinore@alumno.buap.mx>
2026-03-13 12:58:03 -0600

    refactor: configuracionApi.js

--- sistema-monitoreo/src/api/configuracionApi/configuracionApi.js
+++ sistema-monitoreo/src/api/configuracionApi/configuracionApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const clienteApi = axios.create({
-    baseURL: 'http://127.0.0.1:8001/api', // http://localhost:3001
+    baseURL: import.meta.env.VITE_BACKEND_DIR, // http://localhost:3001
     headers: {
         'Content-Type': 'application/json'
     }
