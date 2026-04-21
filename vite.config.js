837893509c0e70992b7ebbd3fbc435b94f6dbfff
Luis Alfredo Monroy Villegas <lur0y@hotmail.com>
2026-04-21 11:11:07 -0600

    Se crea el comando host

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from 'path'
+import {defineConfig} from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@hooks': path.resolve(__dirname, 'src/hooks'),
-      '@helpers': path.resolve(__dirname, 'src/helpers'),
-      '@assets': path.resolve(__dirname, 'src/assets'),
-      '@pages': path.resolve(__dirname, 'src/pages'),
-      '@context': path.resolve(__dirname, 'src/context'),
-      '@styles': path.resolve(__dirname, 'src/styles'),
-      '@tests': path.resolve(__dirname, 'src/tests'),
-      '@api': path.resolve(__dirname, 'src/api'),
-      '@const': path.resolve(__dirname, 'src/const'),
-      '@lib': path.resolve(__dirname, 'src/lib'),
-    },
-  },
-})
+	server: {
+		port: 91,
+	},
+	plugins: [react()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "src"),
+			"@components": path.resolve(__dirname, "src/components"),
+			"@hooks": path.resolve(__dirname, "src/hooks"),
+			"@helpers": path.resolve(__dirname, "src/helpers"),
+			"@assets": path.resolve(__dirname, "src/assets"),
+			"@pages": path.resolve(__dirname, "src/pages"),
+			"@context": path.resolve(__dirname, "src/context"),
+			"@styles": path.resolve(__dirname, "src/styles"),
+			"@tests": path.resolve(__dirname, "src/tests"),
+			"@api": path.resolve(__dirname, "src/api"),
+			"@const": path.resolve(__dirname, "src/const"),
+			"@lib": path.resolve(__dirname, "src/lib"),
+		},
+	},
+});
