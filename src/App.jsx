b04d87acbd8bffcb7ca781f3b31351fbfce991f9
Luis Alfredo Monroy Villegas <lur0y@hotmail.com>
2026-04-21 13:41:02 -0600

    Se habilita modo oscuro por defecto

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,22 @@ import { lightTheme, darkTheme } from '@styles/tema/tema.js';
 import { useState } from 'react';
 
 function App() {
-    const [isDarkMode, setIsDarkMode] = useState(false);
-    return (
-        <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
-            <CssBaseline />
-            <DashboardPage toggleTheme={() => setIsDarkMode(!isDarkMode)} isDarkMode={isDarkMode} />
-        </ThemeProvider>
-    );
+	const storedTheme = localStorage.getItem('theme');
+	const defaultTheme = storedTheme ? storedTheme : 'dark';
+	const [isDarkMode, setIsDarkMode] = useState(defaultTheme === 'dark');
+
+	function toggleTheme() {
+		const newTheme = isDarkMode ? 'light' : 'dark';
+		localStorage.setItem('theme', newTheme);
+		setIsDarkMode(newTheme === 'dark');
+	}
+
+	return (
+		<ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
+			<CssBaseline />
+			<DashboardPage toggleTheme={toggleTheme} isDarkMode={isDarkMode} />
+		</ThemeProvider>
+	);
 }
 
 export default App;
--- src/lib/echo.js
+++ src/lib/echo.js
@@ -1,14 +1,14 @@
-import Echo from 'laravel-echo';
-import Pusher from 'pusher-js';
+import Echo from "laravel-echo";
+import Pusher from "pusher-js";
 
 window.Pusher = Pusher;
 
 export const echo = new Echo({
-    broadcaster: 'reverb',
-    key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST,
-    wsPort: import.meta.env.VITE_REVERB_PORT,
-    wssPort: import.meta.env.VITE_REVERB_PORT,
-    forceTLS: import.meta.env.VITE_FORCE_TLS,
-    enabledTransports: ['ws', 'wss'],
+	broadcaster: "reverb",
+	key: import.meta.env.VITE_REVERB_APP_KEY,
+	wsHost: import.meta.env.VITE_REVERB_HOST,
+	wsPort: import.meta.env.VITE_REVERB_PORT,
+	wssPort: import.meta.env.VITE_REVERB_PORT,
+	forceTLS: import.meta.env.VITE_REVERB_FORCE_TLS,
+	enabledTransports: ["ws", "wss"],
 });
