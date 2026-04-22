364cf22bb10a8192af86489907bc5aa322eb8daa
Luis Alfredo Monroy Villegas <lur0y@hotmail.com>
2026-04-22 16:23:11 -0600

    Se ajjusta diseño a mobile

--- src/pages/Monitoreo/components/ResumenServidores/ResumenServidores.jsx
+++ src/pages/Monitoreo/components/ResumenServidores/ResumenServidores.jsx
@@ -4,21 +4,30 @@ import StorageIcon from '@mui/icons-material/Storage';
 import { estilosResumen } from '@pages/Monitoreo/styles/ResumenServidoresStyle/resumenStyles.jsx';
 
 const ResumenServidores = ({ total = 0 }) => {
-    return (
-        <Paper elevation={3} sx={estilosResumen.contenedorPrincipal}>
-            <Box>
-                <Typography variant="body1" sx={estilosResumen.tituloSeccion}>
-                    Total Servidores
-                </Typography>
-
-                <Typography sx={estilosResumen.valorGrande}>
-                    {total}
-                </Typography>
-            </Box>
-
-            <StorageIcon sx={estilosResumen.icono} />
-        </Paper>
-    );
+	return (
+		<Paper elevation={3} sx={estilosResumen.contenedorPrincipal}>
+			<Box
+				sx={{
+					display: 'flex',
+					flexDirection: 'row',
+					alignItems: 'center',
+					justifyContent: 'space-between',
+					width: '100%',
+					height: '100%',
+				}}
+			>
+				<Box sx={{ flex: 1 }}>
+					<Typography variant="body1" sx={estilosResumen.tituloSeccion}>
+						Total Servidores
+					</Typography>
+					<Typography sx={estilosResumen.valorGrande}>
+						{total}
+					</Typography>
+				</Box>
+				<StorageIcon sx={estilosResumen.icono} />
+			</Box>
+		</Paper>
+	);
 };
 
 export default ResumenServidores;
